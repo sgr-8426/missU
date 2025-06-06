85dc223bb562b54adb14e5ea84716e8022a33f1b
Abhi <mandaliyaankit1@gmail.com>
2025-06-06 17:00:49 +0530

    complete hoem page

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,14 +1,33 @@
 export function Homepage() {
     return(
         <>
+          <Nav />
+          <div>
+            <h2>
+                Search  user by username
+            </h2>
+            <form>
+                <input type="text" placeholder="Enter username" />
+                <button type="submit">Search</button>
+            </form>
+          </div>
+          <div>
+            <h2>searched user details</h2>
+            <div></div>
+          </div>
+         </>
+    )
+}
+export function Nav(){
+    return(
+        <>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
             </ul>
           </nav>
-          
-         </>
+        </>
     )
 }
