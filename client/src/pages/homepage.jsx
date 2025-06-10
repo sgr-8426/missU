d1b864c215e0b0fa3868f502a729dc7163b05f21
Abhi <mandaliyaankit1@gmail.com>
2025-06-10 13:18:06 +0530

    home page

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,33 +1,35 @@
 import "./homepage.css";
 export function HomePage() {
-    return(
-        <>
-          <Nav />
-          <div className="search-user"> 
-            <h2>
-                Search  user by username
-            </h2>
-            <Input type="text" placeholder="Enter username" />
-                <button type="submit" id ="btn">Search</button>
-          </div>
-          <div className="searched-user-details">
-            <h2>Searched user details</h2>
-            <div></div>
-          </div>
-         </>
-    )
+  return (
+    <>
+      <Nav />
+      <div className="search-user">
+        <h2>Search user by username</h2>
+        <div className="inputandbutton">
+          <input type="text" placeholder="Enter username" />
+          <button type="submit" id="btn">
+          Search
+        </button>
+        </div>
+      </div>
+      <div className="searched-user-details">
+        <h2>Searched user details</h2>
+        <div className="show-user-details">  </div>
+      </div>
+    </>
+  );
 }
-export function Nav(){
-    return(
-        <>
-          <nav className="navbar">
-            <h1 className="logo">missU</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        </>
-    )
+export function Nav() {
+  return (
+    <>
+      <nav className="navbar">
+        <ul>
+          <li className="logo">missU</li>
+          <li className="home">Home</li>
+          <li className="about">About</li>
+          <li className="contact">Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
 }
